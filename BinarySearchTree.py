b3e8d66aa68abb556b9dfc8c8fe0f9a140ec6e93
Yonathan <107772898+yyonathan@users.noreply.github.com>
2024-03-30 00:39:02 -0400

    Just Saving

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -6,10 +6,43 @@ class Node:
         self.val = key
 
 #Returns Largest Elements
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+
+
 def find_largest (root_node):
     current = root_node
     if current:
         while current.right is not None:
             current = current.right
         return current.val
+def find_sum(root_node):
+    if root_node == None:
+        return 0
+    return root_node.val + find_sum(root_node.left) \
+                        + find_sum(root_node.right)
+
+
+
+
+
+
+y = Node(70)
+while True:
+    num = int(input("\n\tEnter an Integer e.g 25 ===>"))
+    if num == 0:
+        break
+    r = insert(r,num)
+
+print("\n\tLargest Element")
 
